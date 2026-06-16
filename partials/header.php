e70a081813bbd42e8295632652c9ccd223fb2601
develakoulis <80943397+develakoulis@users.noreply.github.com>
2026-06-16 16:14:12 +0100

    a

--- partials/header.php
+++ partials/header.php
@@ -48,7 +48,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="/case-studies/index.php">
+          <a class="nav-link" href="/case-studies/">
             Case Studies
           </a>
         </li>
